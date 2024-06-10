1eb440e7c5fc86fb03d9f8c9e06863253da01a55
gary2504 <garychaves25@gmail.com>
2024-06-09 23:22:13 -0600

    agregue funcion para agregar los elementos a las tareas (checkbox, texto y boton de eliminar

--- src/index.js
+++ src/index.js
@@ -2,44 +2,75 @@ import { fetchtarea } from "./indexdos.js";
 import { guardarPost } from "./indextres.js";
 import { borrarTareas } from "./indexcuatro.js"; 
 import { actualizarTarea } from "./indexcinco.js";
+const apiUrl = "http://localhost:3000/api/todo"; //variable con la url de la api
 
-const apiUrl = "http://localhost:3000/api/todo";
+
 let inputTarea = document.getElementById("inputTarea");
 let botonTarea = document.getElementById("botonTarea");
 let contenedortareas = document.getElementById("contenedortareas");
-const agregarTarea = async () => {
+let numeroTareas = document.getElementById("numerotareas");
+let botonEliminarCompletadas = document.getElementById("botonEliminarCompletadas"); //para eliminar las tareas completadas
+let contadorCompletadas = 0;
+
+const agregarTarea = async () => { //agrega una nueva tarea 
     const textoTarea = inputTarea.value.trim();
     if (textoTarea === '') {
-        alert('Ingrese un texto');
+        alert('Ingrese un texto'); //si presiono el boton y no hay una tarea entonces mostrará una alerta
         return;
     }
-    const nuevaTarea = await guardarPost(apiUrl, textoTarea);
+
+    //guardará las nuevas tareas en el server
+    const nuevaTarea = await guardarPost("http://localhost:3000/api/todo", textoTarea);
     if (nuevaTarea) {
         const tareaItem = crearTareaItem(nuevaTarea);
-        contenedortareas.appendChild(tareaItem);
+        contenedortareas.appendChild(tareaItem); //agregara nueva tarea a contenedortareas
         inputTarea.value = '';
         if (contenedortareas.children.length === 1) {
             ocultarMensajeNoTareas();
         }
     }
+    
 };
-const mostrarMensajeNoTareas = () => {
-    const mensajeNoTareas = document.createElement('p');
-    mensajeNoTareas.id = 'mensajeNoTareas';
-    mensajeNoTareas.textContent = 'No existen tareas';
-    contenedortareas.appendChild(mensajeNoTareas);
-};
-const ocultarMensajeNoTareas = () => {
-    const mensajeNoTareas = document.getElementById('mensajeNoTareas');
-    if (mensajeNoTareas) {
-        mensajeNoTareas.remove();
-    }
-};
-botonTarea.addEventListener('click', agregarTarea);
-inputTarea.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        agregarTarea();
-    }
-});
+
+//funcion para crear un elemento a las tareas
+const crearTareaItem = (tarea) => {
+    const tareaItem = document.createElement('div'); //contenedor para tarea
+    tareaItem.classList.add('tareaitem');
+    tareaItem.dataset.id = tarea.id; //guardar el id de la tarea
+    const selectCheckbox = document.createElement('input');
+    selectCheckbox.type = 'checkbox'; //checkbox para marcar cuando una tarea esta terminada
+    selectCheckbox.addEventListener('change', async () => {
+        const completed = selectCheckbox.checked;
+        await actualizarTarea("http://localhost:3000/api/todo", tareaItem.dataset.id, completed);
+        if (completed) {
+            tareaItem.classList.add('completed'); //marcara la tarea como completada
+            contadorCompletadas++;
+            console.log(`Tarea completada: ${tareaItem.dataset.id}`); //mostrara la tarea y el id
+        } else {
+            tareaItem.classList.remove('completed');
+            contadorCompletadas--; //para quitar la tarea como completada
+        }
+        actualizarContadorCompletadas();
+    });
+    
+    const p = document.createElement('p'); //crear elemento p para el texto de la tarea
+    p.textContent = tarea.task;
+    p.id = tarea.id;
+    const botonEliminar = document.createElement('button'); //boton para eliminar cada tarea
+    botonEliminar.innerText = '🗑️';
+    botonEliminar.classList.add('botoneliminar');
+    botonEliminar.addEventListener('click', async () => {
+        await borrarTareas("http://localhost:3000/api/todo", tareaItem.dataset.id); //elimina la tarea del server
+        tareaItem.remove(); //elimina la tarea de la pagina
+        if (contenedortareas.children.length === 0) {
+            mostrarMensajeNoTareas();
+        } 
+    
+    });
+
+    tareaItem.append(selectCheckbox, p, botonEliminar); //agrega checkbox, la tarea y el boton de eliminar
+    return tareaItem;
+
+}
 
 
